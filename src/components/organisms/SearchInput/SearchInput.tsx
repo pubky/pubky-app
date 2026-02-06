32c882ad12989dc287aefc92bef1a8f93fa848c9
Nirzhuk <john.voiden@gmail.com>
2026-02-06 11:54:04 +0100

    fix: issue 1192 - autoFocus when we dont have any tags in search route

--- src/components/organisms/SearchInput/SearchInput.tsx
+++ src/components/organisms/SearchInput/SearchInput.tsx
@@ -10,7 +10,10 @@ import { APP_ROUTES } from '@/app/routes';
 import { CLICKABLE_TAGS_DEFAULT_MAX_LENGTH } from '@/config/tags';
 import { parseTagsFromUrl } from './SearchInput.utils';
 
-export function SearchInput() {
+interface SearchInputProps {
+  autoFocus?: boolean;
+}
+export function SearchInput({ autoFocus = false }: SearchInputProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -86,6 +89,7 @@ export function SearchInput() {
         onInputChange={handleInputChange}
         onKeyDown={handleKeyDown}
         onFocus={handleFocus}
+        autoFocus={autoFocus}
       />
 
       {/* Suggestions dropdown */}
@@ -94,7 +98,6 @@ export function SearchInput() {
           id={suggestionsId}
           aria-label="Search suggestions"
           hotTags={hotTags}
-          inputValue={inputValue}
           hasInput={hasInput}
           autocompleteTags={autocompleteTags}
           autocompleteUsers={autocompleteUserData}
@@ -102,7 +105,6 @@ export function SearchInput() {
           recentTags={recentTags}
           onTagClick={handleTagClick}
           onUserClick={handleUserClick}
-          onSearchAsTagClick={handleTagClick}
           onClearRecentSearches={clearRecentSearches}
         />
       )}
