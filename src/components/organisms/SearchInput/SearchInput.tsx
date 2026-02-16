56c808b5c1f0cbdd1ce324844636deeaaa8f6162
Nirzhuk <john.voiden@gmail.com>
2026-02-16 16:56:18 +0100

    feat(SearchInput): integrate mobile detection for improved UX
    - Added mobile detection using `useIsMobile` hook in SearchInput and Search components.
    - Adjusted autoFocus behavior in SearchInput based on mobile state.
    - Enhanced tag handling to account for mobile view, improving user interaction on mobile devices.

--- src/components/organisms/SearchInput/SearchInput.tsx
+++ src/components/organisms/SearchInput/SearchInput.tsx
@@ -18,6 +18,7 @@ export function SearchInput({ autoFocus = false }: SearchInputProps) {
 
   const { addTagToSearch, removeTagFromSearch, activeTags, isReadOnly } = Hooks.useTagSearch();
   const { setActiveTags, recentUsers, recentTags, addUser, clearRecentSearches } = Core.useSearchStore();
+  const isMobile = Hooks.useIsMobile();
 
   const handleEnter = (value: string) => {
     addTagToSearch(value, { addToRecent: true });
@@ -62,6 +63,9 @@ export function SearchInput({ autoFocus = false }: SearchInputProps) {
   const handleTagClick = (tag: string) => {
     addTagToSearch(tag, { addToRecent: true });
     clearInputValue();
+    if (isMobile) {
+      setFocus(false);
+    }
     if (pathname !== APP_ROUTES.SEARCH) {
       setFocus(false);
     }
