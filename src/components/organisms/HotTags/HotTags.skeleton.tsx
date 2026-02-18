199afd3733fc693e3b01a87abd91c9f9d2d61a40
Orlando Goncalves <orlando.goncalves@gmail.com>
2026-02-18 15:40:37 +0100

    feat(ui): add skeleton for profile Tagged as section, remove unused data-cy
    - Replace loading spinner with ProfilePageTaggedAsSkeleton in Tagged as sidebar
    - Add colocated ProfilePageTaggedAs.skeleton.tsx with 3 skeleton rows
    - Remove unused data-cy from ProfilePageTaggedAs and HotTags skeletons

--- src/components/organisms/HotTags/HotTags.skeleton.tsx
+++ src/components/organisms/HotTags/HotTags.skeleton.tsx
@@ -3,7 +3,7 @@ import { MAX_TAGS } from './HotTags.constants';
 
 export function HotTagsSkeleton() {
   return (
-    <Atoms.Container overrideDefaults className="flex w-full flex-col gap-2" data-cy="hot-tags-skeleton-list">
+    <Atoms.Container overrideDefaults className="flex w-full flex-col gap-2">
       {Array.from({ length: MAX_TAGS }).map((_, index) => (
         <Atoms.Skeleton key={`hot-tag-skeleton-${index}`} className="h-8 w-full rounded-md" />
       ))}
