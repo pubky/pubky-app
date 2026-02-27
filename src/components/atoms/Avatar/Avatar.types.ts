fee86e4adefde4e0a84dff21cd71813b538816bf
secondl1ght <secondl1ght@protonmail.com>
2026-02-27 04:55:21 +0000

    build: migrate to unified radix package

--- src/components/atoms/Avatar/Avatar.types.ts
+++ src/components/atoms/Avatar/Avatar.types.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as AvatarPrimitive from '@radix-ui/react-avatar';
+import { Avatar as AvatarPrimitive } from 'radix-ui';
 import { type VariantProps } from 'class-variance-authority';
 import { avatarVariants } from './Avatar.variants';
 
