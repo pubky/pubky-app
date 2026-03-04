d31f1f104f68a7432a3f2e2c51b3bb63224992f3
Nirzhuk <john.voiden@gmail.com>
2026-03-04 17:14:24 +0100

    refactor(ProfilePageLayoutWrapper, ContentLayout, Scan, MobileFooter): update max-width and snapshot tests
    - Updated the max-width class for ProfilePageLayoutWrapper and ContentLayout to 1200px for improved layout consistency.
    - Adjusted snapshot tests for ProfilePageLayoutWrapper, ContentLayout, and Scan components to reflect the new styling changes.
    - Enhanced MobileFooter styling with transition effects and transform properties to maintain visual integrity.

--- src/components/molecules/ProfilePageLayoutWrapper/ProfilePageLayoutWrapper.tsx
+++ src/components/molecules/ProfilePageLayoutWrapper/ProfilePageLayoutWrapper.tsx
@@ -6,10 +6,7 @@ export interface ProfilePageLayoutWrapperProps {
 
 export function ProfilePageLayoutWrapper({ children }: ProfilePageLayoutWrapperProps) {
   return (
-    <Atoms.Container
-      overrideDefaults={true}
-      className="mx-auto mt-6 w-full max-w-sm px-6 pt-0 sm:max-w-xl md:max-w-3xl lg:max-w-5xl xl:mt-0 xl:max-w-6xl xl:px-0"
-    >
+    <Atoms.Container overrideDefaults={true} className="mx-auto mt-6 w-full max-w-[1200px] px-6 pt-0 xl:px-0">
       {children}
     </Atoms.Container>
   );
