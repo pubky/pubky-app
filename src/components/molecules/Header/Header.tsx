1a1ad2bc7cc76dfe7f65a617bf27520ad75f3135
Nirzhuk <john.voiden@gmail.com>
2026-03-05 14:32:09 +0100

    fix: fix tests and minor tweaks in css

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -31,7 +31,7 @@ export const HeaderContainer = ({ children, className, classNameNav }: HeaderCon
         size="container"
         className={Libs.cn(
           'pointer-events-auto mx-auto flex h-24 w-full flex-row flex-wrap items-center justify-between gap-4 sm:flex-nowrap sm:gap-6',
-          'sm:p-auto p-4',
+          'p-4 sm:p-0',
           classNameNav,
         )}
       >
